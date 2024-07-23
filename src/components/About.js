c0f41cbc752a102694732ff57e7bc77b6336d090
hui051875 <hui051875@gmail.com>
2024-07-23 14:57:26 +0900

    add video buttons

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,8 @@
-import { useEffect } from 'react';
+import classNames from 'classnames';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 const Div = styled.div`
   margin-top: 5rem;
@@ -22,31 +24,90 @@ const Div = styled.div`
     font-family: 'Josefin Sans', 'Noto Sans KR', sans-serif;
     font-style: italic;
   }
+
+  #buttons {
+    display: flex;
+    flex-flow: row;
+    align-items: center;
+  }
+
+  .button {
+    width: 40px;
+    height: 40px;
+    line-height: 40px;
+    border-radius: 15px;
+    margin-left: 8px;
+    margin-right: 8px;
+    background-color: black;
+    font-family: Arvo, 'Noto Sans KR', sans-serif;
+    color: white;
+
+    background-color: rgb(30, 30, 30);
+
+    &:hover {
+      cursor: pointer;
+    }
+
+    &:hover.inactive {
+      cursor: pointer;
+      background-color: rgb(45, 45, 45);
+    }
+  }
+
+  .active {
+    background-color: grey;
+  }
+
+  .carousel-item {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
 `;
 
 function About() {
   const width = '85vw';
   const half = '40vw';
-  const outerWidth = '90vw';
+
+  const [cur, setCur] = useState(0);
 
   useEffect(() => {
+    const buttons = ['button1', 'button2', 'button3'].map((b) =>
+      document.getElementById(b)
+    );
+
     const elems = ['card1', 'card2', 'card3'].map((id) =>
       document.getElementById(id)
     );
-    const w = elems[0].clientWidth;
-    const d = document.getElementById('carousel');
-    d.scrollLeft = w * 0.5;
 
-    for (const i in elems) {
-      const next = (i + 1) % elems.length;
-      elems[i].addEventListener('ended', () => {
-        d.scrollTo({ left: w * (next + 0.5), behavior: 'smooth' });
-        elems[next].play();
-      });
+    let now = 0;
+    let setNow = (i) => {
+      now = i;
+      setCur(i);
+    };
+
+    const moveTo = (i) => {
+      setNow(i);
+      elems[i].currentTime = 0;
+      elems[i].play();
+      const carousel = document.getElementById('carousel');
+      const width = document.getElementById('card1').clientWidth;
+      carousel.scrollTo({ left: width * (i + 0.5), behavior: 'smooth' });
+    };
+
+    moveTo(0);
+
+    for (let i = 0; i < 3; i++) {
+      buttons[i].addEventListener('click', () => moveTo(i));
     }
 
-    elems[0].play();
-  });
+    for (let i = 0; i < 3; i++) {
+      elems[i].addEventListener('ended', () => {
+        if (now === i) {
+          moveTo((i + 1) % elems.length);
+        }
+      });
+    }
+  }, []);
 
   const [t] = useTranslation();
 
@@ -57,7 +118,6 @@ function About() {
       <div
         id="carousel"
         className="mainVideo carousel carousel-center space-x-4 p-4"
-        style={{ width: outerWidth }}
       >
         <div className="carousel-item" style={{ width: half }}></div>
         <div className="carousel-item">
@@ -94,6 +154,47 @@ function About() {
           ></video>
         </div>
         <div className="carousel-item" style={{ width: half }}></div>
+      </div>
+
+      <div id="buttons">
+        <motion.div
+          whileHover={{ scale: 1.1 }}
+          whileTap={{
+            scale: 0.9,
+          }}
+          id="button1"
+          className={classNames('button', {
+            active: cur === 0,
+          })}
+        >
+          1
+        </motion.div>
+        <motion.div
+          whileHover={{ scale: 1.1 }}
+          whileTap={{
+            scale: 0.95,
+          }}
+          id="button2"
+          className={classNames('button', {
+            active: cur === 1,
+            inactive: cur !== 1,
+          })}
+        >
+          2
+        </motion.div>
+        <motion.div
+          whileHover={{ scale: 1.1 }}
+          whileTap={{
+            scale: 0.9,
+          }}
+          id="button3"
+          className={classNames('button', {
+            active: cur === 2,
+            inactive: cur !== 2,
+          })}
+        >
+          3
+        </motion.div>
       </div>
     </Div>
   );
